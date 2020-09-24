5314b197323d05b80e3ed7afa03a8d7cbee621ab
Vishal Vijay <0vishalvijay0@gmail.com>
2020-09-24 20:32:15 +0530

    Setup theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
 import App from "./App";
+import theme from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
