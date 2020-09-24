d1f0ce9ff194b9fac6f4716f5a9183805f2f5f0a
Vishal Vijay <0vishalvijay0@gmail.com>
2020-09-24 20:28:33 +0530

    Add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
+import { Box, Typography } from "@material-ui/core";
+import Container from "@material-ui/core/Container/Container";
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App sample
+        </Typography>
+      </Box>
+    </Container>
   );
 };
 
